99ad24414d422ccb86030690cce07437ab8d4a01
Eric Silva <ericsilvaccp@gmail.com>
2020-07-06 13:47:12 -0300

    correcting error in the build process, support for decorators

--- babel.config.js
+++ babel.config.js
@@ -16,9 +16,11 @@ module.exports = {
           '@controllers': './src/source/controllers/',
           '@services': './src/source/services/',
           '@entitys': './src/database/entitys/',
-          '@logger': './src/common/logger/index.ts'
+          '@logger': './src/common/logger/index.js'
         }
-      }]
+      }],
+      ["@babel/plugin-proposal-decorators", { "legacy": true }],
+      ["@babel/plugin-proposal-class-properties", { "loose": true }]
     ],
     ignore: [
       '**/*.spec.ts'
--- src/database/index.ts
+++ src/database/index.ts
@@ -6,41 +6,34 @@ import { config } from 'dotenv';
 import { resolve } from 'path';
 
 class ConnectionFactory {
-    private instance: undefined | Connection;
-    async createConnection ():Promise<Connection> {
-      config();
-      const {
-        DATABASE_HOST,
-        DATABASE_USER,
-        DATABASE_PASS,
-        DATABASE_NAME,
-        DATABASE_PORT,
-        MODE
-      } = process.env;
+  async createConnection () {
+    config();
+    const {
+      DATABASE_HOST,
+      DATABASE_USER,
+      DATABASE_PASS,
+      DATABASE_NAME,
+      DATABASE_PORT,
+      MODE
+    } = process.env;
 
-      try {
-        const connection = await createConnectionTypeORM({
-          type: 'postgres',
-          host: DATABASE_HOST,
-          username: DATABASE_USER,
-          password: DATABASE_PASS,
-          database: DATABASE_NAME,
-          port: Number(DATABASE_PORT),
-          synchronize: (MODE === 'development'),
-          migrationsRun: (MODE === 'development'),
-          entities: [resolve(__dirname, '.', 'entitys', '*.entity{.ts,.js}')]
-        });
-
-        if (this.instance === undefined) {
-          this.instance = connection;
-        }
-
-        return this.instance;
-      } catch (error) {
-        console.log('not Connected database - ' + error);
-        throw new Error('Not connected database');
-      }
+    try {
+      createConnectionTypeORM({
+        type: 'postgres',
+        host: DATABASE_HOST,
+        username: DATABASE_USER,
+        password: DATABASE_PASS,
+        database: DATABASE_NAME,
+        port: Number(DATABASE_PORT),
+        synchronize: (MODE === 'development'),
+        migrationsRun: (MODE === 'development'),
+        entities: [resolve(__dirname, '.', 'entitys', '*.entity{.ts,.js}')]
+      });
+    } catch (error) {
+      console.log('not Connected database - ' + error);
+      throw new Error('Not connected database');
     }
+  }
 }
 
 export const createConnectionFactoty = new ConnectionFactory().createConnection;
